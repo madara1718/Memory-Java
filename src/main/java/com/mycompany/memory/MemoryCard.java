e531b329b3beff944bec88891b11950172ddac7c
kijan <kijan34520@gmail.com>
2023-03-19 00:07:14 -0600

    gg

---
+++ src/main/java/com/mycompany/memory/MemoryCard.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.memory;
+
+/**
+ *
+ * @author Kijan
+ */
+public class MemoryCard {
+    private int id;
+    private String image;
+    public MemoryCard(int id, String image){
+        this.id=id;
+        this.image=image;
+    }
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    
+}
